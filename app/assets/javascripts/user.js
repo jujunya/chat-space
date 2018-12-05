fcabcd99a90c10fd2764df2c594e3ad3d0d70b49
junya <junya@live.jp>
2018-12-05 10:37:22 +0900

    fix-javascript

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,25 +3,25 @@ $(function() {
   
   var search_list = $("#user-search-result");
   function appendUser(user){
-    var html = '<div class="chat-group-user clearfix">'+
-                '<p class="chat-group-user__name">'+ user.name +'</p>'+
-                '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="'+ user.id +'" data-user-name="'+ user.name+ '">'+ 追加 + '</a>'+
-                '</div>';
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
     search_list.append(html);
   }
   function appendNoUser(user) {
-    var html = '<div class="chat-group-user clearfix">'+
-                  '<p class="chat-group-user__name">'+ group.users.name + '</p>'+
-                '</div>';
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${group.users.name}</p>
+                </div>`
     search_list.append(html);
   }
   var user_list = $('#chat-group-users');
   function addUser(user_id,user_name){
-    var html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user">'+
-                  '<input name="group[user_ids][]" type="hidden" value="'+ user.id +'">'+
-                   '<p class="chat-group-user__name">' + user_name + '</p>'+
-                  '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">'+削除 + '</a>'+
-                '</div>';
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user">
+                  <input name="group[user_ids][]" type="hidden" value="${user_id}">
+                   <p class="chat-group-user__name">${user_name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+                </div>`
     user_list.append(html);
   }
   
