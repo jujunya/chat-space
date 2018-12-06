714f5a96a279d470b6ee6b9f42e2e306f3bb493a
junya <junya@live.jp>
2018-12-06 14:26:42 +0900

    fix-carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1',
-    bucket: 'baketsjun2'
+    bucket: 'testbuckets-j'
   }
 
   config.fog_directory  = 'testbuckets-j'
